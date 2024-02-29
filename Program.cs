5e911efd6322bc26b79036c6f6134ea4a4ec1826
rikif <rikif@LAPTOP-P9LPHMTP>
2024-02-29 09:13:39 +0100

    feat: abbiamo messo in tabella il voto e il suo peso

--- Program.cs
+++ Program.cs
@@ -10,12 +10,14 @@ namespace as2324._3G.FucciRiccardo.PSArray
             int dim=Convert.ToInt32(Console.ReadLine());
             int[] array= new int[dim];
             double[] voti= new double[dim];
-            int[]pesi= new int[dim];
+            int[] pesi = new int[dim];
             
+
+            CaricaVettori(voti, pesi, nVoti);
         }
         static void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
         {
-            Console.WriteLine("");
+            Console.WriteLine("voto" + " pesi");
             for(int i = 0; i < voti.Length; i++)
             {
                 Console.WriteLine($"voto{i}     pesi{i}");
@@ -29,6 +31,16 @@ namespace as2324._3G.FucciRiccardo.PSArray
             {
                  voti[i]=random.Next(0,11);
                  pesi[i]=random.Next(0,101);
+            }
+        }
+        static void StampaVotiDispariMaggiori4(ref double[] voti, ref int[] pesi, int nVoti)
+        {
+            for(int i = 0; i<nVoti; i++)
+            {
+                if(voti[i] > 4 &&  )
+                {
+                     
+                }
             }
         }
 

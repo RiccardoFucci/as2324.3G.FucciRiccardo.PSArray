fa420575f8fcfd16724a220e5222bac475ef9c3d
rikif <rikif@LAPTOP-P9LPHMTP>
2024-02-29 09:32:23 +0100

    feat:ora si inizia a fare la funzione per la media ponderata

--- Program.cs
+++ Program.cs
@@ -7,49 +7,54 @@ namespace as2324._3G.FucciRiccardo.PSArray
         static void Main(string[] args)
         {
             Console.WriteLine("inserire la dimensione");
-            int dim=Convert.ToInt32(Console.ReadLine());
-            int[] array= new int[dim];
-            double[] voti= new double[dim];
+            int dim = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[dim];
+            double[] voti = new double[dim];
             int[] pesi = new int[dim];
-            
-
-            CaricaVettori( voti, pesi, nVoti);
-        }
-        static void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
-        {
-            Console.WriteLine("voto" + " pesi");
-            for(int i = 0; i < voti.Length; i++)
+            StampaVotiPesi(voti, pesi, nVoti);
+            CaricaVettori(voti, pesi, nVoti);
+            StampaVotiDispariMaggiori4(voti, pesi, nVoti);
             {
-                Console.WriteLine($"voto{i}     pesi{i}");
             }
-
-        }
-        static void CaricaVettori(ref double[] voti, ref int[] pesi, int nVoti)
-        {
-            Random random = new Random();
-            for(int i = 0;i < nVoti; i++)
+            static void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
             {
-                 voti[i]=random.Next(0,11);
-                 pesi[i]=random.Next(0,101);
-            }
-        }
-        static void StampaVotiDispariMaggiori4(ref double[] voti, ref int[] pesi, int nVoti)
-        {
-            for(int i = 0; i<nVoti; i++)
-            {
-                if (voti[i] > 4)
+                Console.WriteLine("voto" + " pesi");
+                for (int i = 0; i < voti.Length; i++)
                 {
-                    Console.WriteLine($"in posizione{i+1} c'è il voto{voti} con peso{pesi}");
+                    Console.WriteLine($"voto{i}     pesi{i}");
+                }
+
+            }
+            static void CaricaVettori(ref double[] voti, ref int[] pesi, int nVoti)
+            {
+                Random random = new Random();
+                for (int i = 0; i < nVoti; i++)
+                {
+                    voti[i] = random.Next(0, 11);
+                    pesi[i] = random.Next(0, 101);
                 }
             }
-        }
-       static void MediaPonderata(double[] voti, int[] pesi,int nVoti,ref double max, ref int posmax,ref double min, ref int posmin)
-        {
-            for (int i = 0;i<voti.Length; i++)
+            static void StampaVotiDispariMaggiori4(ref double[] voti, ref int[] pesi, int nVoti)
             {
-
+                for (int i = 0; i < nVoti; i++)
+                {
+                    if (voti[i] > 4)
+                    {
+                        Console.WriteLine($"in posizione{i + 1} c'è il voto{voti} con peso{pesi}");
+                    }
+                }
             }
-        }
+            static void MediaPonderata(double[] voti, int[] pesi, int nVoti, ref double max, ref int posmax, ref double min, ref int posmin)
+            {
+                for (int i = 0; i < voti.Length; i++)
+                {
+                    if (voti[i] > max)
+                    {
 
+                    }
+                }
+            }
+
+        }
     }
 }
